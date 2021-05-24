1b7c16a641979d8836fc7be7fd623752c4223818
Matheus Frata <git@mfrata.com>
2021-05-24 18:42:41 -0300

    Add initial python files

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="dsalgo",
+    version="0.1.0",
+    packages=setuptools.find_packages(exclude=['tests*']),
+    description="My implemetations of ds and algo in python",
+    author="@mfrata",
+)
