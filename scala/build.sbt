d96dedb296ad7c7968cca2bfefbe0fb87d933f0f
Matheus Frata <git@mfrata.com>
2021-07-21 10:21:40 -0300

    Add base files for scala

---
+++ scala/build.sbt
@@ -0,0 +1,6 @@
+name := "DSAlgo"
+version := "0.1.0"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.2.9" % Test
+)
---
+++ scala/src/test/scala/MainTest.scala
@@ -0,0 +1,11 @@
+import org.scalatest._
+import flatspec._
+import matchers._
+
+
+class MainSpec extends AnyFlatSpec with should.Matchers {
+  "This test" should "pass" in {
+    true
+  }
+}
+
